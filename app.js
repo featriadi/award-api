f1d0ac8712c2d2ce5446bed41d74aaca8dad20c4
Aditya Fitriadi <aditya.fitriadi99@gmail.com>
2023-11-02 23:39:08 +0700

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import 'dotenv/config'
 
 // Express
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
+app.use(cors())
 
 // Database
 import { connection } from './db.js'
